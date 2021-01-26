a3198224caca8dd25f20f9a3b62c7ec3195be1b8
exia00r <1700831567@qq.com>
2021-01-26 14:43:08 +0800

    Site updated: 2021-01-26 14:43:07

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","554f56d04ab064db40b02e9e6c42e069"]];
+var precacheConfig = [["/sw-register.js","03864440d0a29b4f88fbaabf26154127"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
