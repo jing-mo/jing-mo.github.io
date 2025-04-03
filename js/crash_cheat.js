7ed19191c36375da2d0854b7c617b543d2f8c7bd
jing-mo <1700831567@qq.com>
2025-04-03 17:08:25 +0800

    Site updated: 2025-04-03 17:08:06

---
+++ js/crash_cheat.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/joke.ico");
+        document.title = '看不见我~看不见我~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = ' ( ๑•̀ㅂ•́) ✧被发现了～';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
